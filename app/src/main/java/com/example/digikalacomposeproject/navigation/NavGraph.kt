38e887af56c4999f6ab353937c152976f67afb9f
mohammad <m.zandieh1999@gmail.com>
2023-03-29 00:08:33 +0330

    s3_navController_pages 1402 / 01 / 07

--- app/src/main/java/com/example/digikalacomposeproject/navigation/NavGraph.kt
+++ app/src/main/java/com/example/digikalacomposeproject/navigation/NavGraph.kt
@@ -4,7 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.example.digikalacomposeproject.ui.screens.SplashScreen
+import com.example.digikalacomposeproject.ui.screens.*
 
 @Composable
 fun SetupNavGraph(navController: NavHostController) {
@@ -15,8 +15,27 @@ fun SetupNavGraph(navController: NavHostController) {
         composable(route = Screen.Splash.route) {
             SplashScreen(navController = navController)
         }
+
+        composable(route = Screen.Home.route) {
+            HomeScreen(navController = navController)
+        }
+
+        composable(route = Screen.Category.route) {
+            CategoryScreen(navController = navController)
+        }
+
+        composable(route = Screen.Basket.route) {
+            BasketScreen(navController = navController)
+        }
+
+        composable(route = Screen.Profile.route) {
+            ProfileScreen(navController = navController)
+        }
+
     }
 }
 
 /**
- * each composable: when we start each screen, which composable Functions should be started*/
+ * 1- each composable: when we start each screen, which composable Functions should be started
+ * 2- explain of input of top composable functions: For example: route = Screen.Profile.route --> it means if in input (route) we have
+ * Screen.Profile.route --> call ProfileScreen(navController = navController) composable function*/
