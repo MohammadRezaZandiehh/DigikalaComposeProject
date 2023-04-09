c9f06537a5e76a177213ae242c6109ac811cfb85
mohammad <m.zandieh1999@gmail.com>
2023-04-09 20:57:55 +0330

    ss10_multi_lang_and_theme 1402 / 01 / 20

--- app/src/main/java/com/example/digikalacomposeproject/ui/screens/BasketScreen.kt
+++ app/src/main/java/com/example/digikalacomposeproject/ui/screens/BasketScreen.kt
@@ -1,25 +1,71 @@
 package com.example.digikalacomposeproject.ui.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
+import com.example.digikalacomposeproject.ui.theme.selectedBottomBar
 
 @Composable
 fun BasketScreen(navController: NavHostController) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Blue),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(text = "BasketScreen")
+    if (isSystemInDarkTheme()) {
+        BasketDark()
+    } else {
+        BasketLight()
     }
+}
+
+@Composable
+fun BasketDark() {
+    Column(
+        modifier =
+        Modifier
+            .fillMaxSize()
+            .background(Color.DarkGray),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "BasketScreen ",
+            color = MaterialTheme.colors.selectedBottomBar
+        )
+    }
+}
+
+@Composable
+fun BasketLight() {
+    Column(
+        modifier =
+        Modifier
+            .fillMaxSize()
+            .background(Color.LightGray),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "BasketScreen ",
+            color = MaterialTheme.colors.selectedBottomBar
+        )
+    }
+}
+
+@Composable
+@Preview
+fun BasketLightPreview() {
+    BasketLight()
+}
+
+@Composable
+@Preview
+fun BasketDarkPreview() {
+    BasketDark()
 }
--- app/src/main/java/com/example/digikalacomposeproject/ui/screens/ProfileScreen.kt
+++ app/src/main/java/com/example/digikalacomposeproject/ui/screens/ProfileScreen.kt
@@ -1,18 +1,33 @@
 package com.example.digikalacomposeproject.ui.screens
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.example.digikalacomposeproject.MainActivity
+import com.example.digikalacomposeproject.util.Constants.ENGLISH_LANG
+import com.example.digikalacomposeproject.util.Constants.PERSIAN_LANG
+import com.example.digikalacomposeproject.viewModel.DataStoreViewModel
 
+@Preview
 @Composable
-fun ProfileScreen(navController: NavHostController) {
+fun ProfileScreen(
+    navController: NavHostController,
+    dataStore: DataStoreViewModel = hiltViewModel()
+) {
 
     Column(
         modifier = Modifier
@@ -21,6 +36,29 @@ fun ProfileScreen(navController: NavHostController) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
+        //get current activity:
+        val activity = LocalContext.current as Activity
+
         Text(text = "ProfileScreen")
+
+        Button(onClick = {
+            dataStore.saveUserLanguage(PERSIAN_LANG)
+            activity.apply {
+                finish()
+                startActivity(Intent(activity, MainActivity::class.java))
+            }
+        }) {
+            Text(text = "fa")
+        }
+
+        Button(onClick = {
+            dataStore.saveUserLanguage(ENGLISH_LANG)
+            activity.apply {
+                finish()
+                startActivity(Intent(activity, MainActivity::class.java))
+            }
+        }) {
+            Text(text = "En")
+        }
     }
 }
