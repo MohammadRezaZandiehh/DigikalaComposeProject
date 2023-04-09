059ef2a3bcb3275097cc5c8388862313757afea3
mohammad <m.zandieh1999@gmail.com>
2023-04-09 17:51:50 +0330

    s8_s9_support_language 1402 / 01 / 20

--- app/src/main/java/com/example/digikalacomposeproject/MainActivity.kt
+++ app/src/main/java/com/example/digikalacomposeproject/MainActivity.kt
@@ -5,11 +5,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.Scaffold
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.digikalacomposeproject.navigation.BottomNavigationBar
 import com.example.digikalacomposeproject.navigation.SetupNavGraph
 import com.example.digikalacomposeproject.ui.theme.DigikalaComposeProjectTheme
+import com.example.digikalacomposeproject.util.Constants
+import com.example.digikalacomposeproject.util.Constants.ENGLISH_LANG
+import com.example.digikalacomposeproject.util.Constants.PERSIAN_LANG
+import com.example.digikalacomposeproject.util.LocaleUtils
 
 class MainActivity : ComponentActivity() {
 
@@ -22,16 +30,20 @@ class MainActivity : ComponentActivity() {
             DigikalaComposeProjectTheme {
                 navController = rememberNavController()
 
-                Scaffold(
-                    bottomBar = {
-                        BottomNavigationBar(
-                            navController = navController,
-                            onItemClick = {
-                                navController.navigate(it.route)
-                            })
+                LocaleUtils.setLocale(LocalContext.current, ENGLISH_LANG)
+
+                CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Ltr ) {
+                    Scaffold(
+                        bottomBar = {
+                            BottomNavigationBar(
+                                navController = navController,
+                                onItemClick = {
+                                    navController.navigate(it.route)
+                                })
+                        }
+                    ) {
+                        SetupNavGraph(navController = navController)
                     }
-                ) {
-                    SetupNavGraph(navController = navController)
                 }
             }
         }
